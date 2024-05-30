8b2e29d268dd002d99d6a6db2d1b2e4318934f06
kenresoft <amadikenneth8@gmail.com>
2024-05-30 13:06:37 +0100

    Update custom_container.dart

--- lib/views/community.dart
+++ lib/views/community.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'collapsed_header.dart';
+import 'custom_container.dart';
+import 'expanded_header.dart';
+
 class Community extends StatefulWidget {
   const Community({super.key});
 
@@ -7,25 +11,74 @@ class Community extends StatefulWidget {
   State<Community> createState() => _CommunityState();
 }
 
-class _CommunityState extends State<Community> with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
+class _CommunityState extends State<Community> {
+  final ScrollController _scrollController = ScrollController();
+  double percent = 0.0;
+  bool showLeadingImage = false;
 
   @override
   void initState() {
-    _controller = AnimationController(vsync: this);
     super.initState();
+    _scrollController.addListener(_updatePercent);
   }
 
   @override
   void dispose() {
-    _controller.dispose();
+    _scrollController.removeListener(_updatePercent);
+    _scrollController.dispose();
     super.dispose();
+  }
+
+  void _updatePercent() {
+    setState(() {
+      double maxScrollExtent = _scrollController.position.maxScrollExtent;
+      double currentScrollOffset = _scrollController.offset;
+      double scrollRange = maxScrollExtent - kToolbarHeight;
+
+      double newPercent = currentScrollOffset / scrollRange;
+      percent = newPercent.clamp(0.0, 1.0);
+      showLeadingImage = percent == 1.0;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: CustomScrollView(
+        controller: _scrollController,
+        slivers: [
+          SliverAppBar(
+            pinned: true,
+            floating: true,
+            expandedHeight: 300.0,
+            toolbarHeight: 75,
+            titleSpacing: 0,
+            title: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 300),
+              switchInCurve: Curves.easeInOut,
+              switchOutCurve: Curves.easeInOut,
+              child: percent == 1.0 ? const CollapsedHeader() : null,
+            ),
+            flexibleSpace: FlexibleSpaceBar(
+              titlePadding: EdgeInsets.zero,
+              title: AnimatedSwitcher(
+                duration: const Duration(milliseconds: 300),
+                switchInCurve: Curves.easeInOut,
+                switchOutCurve: Curves.easeInOut,
+                child: percent != 1.0 ? const ExpandedHeader() : null,
+              ),
+              background: Image.asset('assets/img/header-image.png', fit: BoxFit.cover),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                for (int i = 0; i < 50; i++) CustomContainer(index: i),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
