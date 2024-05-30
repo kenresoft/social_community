5bd497941566f2e1ef965509c38ae321d17f07a3
kenresoft <amadikenneth8@gmail.com>
2024-05-30 13:05:50 +0100

    Update project

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:extensionresoft/extensionresoft.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:social_community/views/community.dart';
-import 'package:social_community/views/testing.dart';
-import 'package:social_community/views/testing2.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -27,12 +25,12 @@ class MyApp extends StatelessWidget {
           title: 'SightGuard',
           theme: ThemeData(
             brightness: Brightness.light,
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+            colorScheme: ColorScheme.fromSeed(
+              seedColor: const Color(0xFFC32422),
+            ),
             fontFamily: 'Proxima Nova',
           ),
-          //home: const Community(),
-          //home: const ExpandingAppBarList(),
-          home: const CustomAppBarExample(),
+          home: const Community(),
         );
       },
     );
