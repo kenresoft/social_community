a366918f3ce33442e2005a911622a04d73cd8518
kenresoft <amadikenneth8@gmail.com>
2024-05-31 06:12:54 +0100

    Update community.dart, add arrow-left.svg and more.svg

--- lib/views/community.dart
+++ lib/views/community.dart
@@ -1,7 +1,5 @@
 import 'package:extensionresoft/extensionresoft.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:social_community/utils/extensions.dart';
 
 import 'collapsed_header.dart';
 import 'custom_container.dart';
@@ -16,12 +14,9 @@ class Community extends StatefulWidget {
 
 class _CommunityState extends State<Community> {
   final ScrollController _scrollController = ScrollController();
-  double position = 0.0;
-
-  //bool showExpanded = true;
-  ScrollDirection scrollDirection = ScrollDirection.idle;
   static const double expandedHeight = 300.0;
   static const double toolbarHeight = 75.0;
+  double scrollPosition = 0.0;
 
   @override
   void initState() {
@@ -39,15 +34,9 @@ class _CommunityState extends State<Community> {
   void _updatePercent() {
     setState(() {
       double currentScrollOffset = _scrollController.offset;
-      scrollDirection = _scrollController.position.userScrollDirection;
       double scrollRange = expandedHeight - toolbarHeight;
 
-      double range = currentScrollOffset / scrollRange;
-      position = range.clamp(0.0, 1.0);
-
-      /*if (scrollDirection == ScrollDirection.reverse && position != 1) {
-        showExpanded = true;
-      }*/
+      scrollPosition = (currentScrollOffset / scrollRange).clamp(0.0, 1.0);
     });
   }
 
@@ -64,15 +53,14 @@ class _CommunityState extends State<Community> {
             toolbarHeight: toolbarHeight,
             titleSpacing: 0,
             title: condition(
-              position == 1.0,
+              scrollPosition == 1.0,
               const CollapsedHeader(),
               null,
             ),
             flexibleSpace: FlexibleSpaceBar(
               titlePadding: EdgeInsets.zero,
               title: condition(
-                /*showExpanded || */
-                /*scrollDirection == ScrollDirection.forward ||*/ position <= 1,
+                scrollPosition <= 1,
                 const ExpandedHeader(),
                 null,
               ),
@@ -82,6 +70,35 @@ class _CommunityState extends State<Community> {
           SliverToBoxAdapter(
             child: Column(
               children: [
+                const SizedBox(
+                  width: 320,
+                  child: Text.rich(
+                    TextSpan(
+                      children: [
+                        TextSpan(
+                          text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod vestibulum lacus, nec consequat nulla efficitur sit amet. Proin eu lorem libero. Sed id enim in urna tincidunt sodales. Vivamus vel semper ame...',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontFamily: 'Proxima Nova',
+                            fontWeight: FontWeight.w400,
+                            height: 0,
+                          ),
+                        ),
+                        TextSpan(
+                          text: 'Read more',
+                          style: TextStyle(
+                            color: Color(0xFFEF456F),
+                            fontSize: 16,
+                            fontFamily: 'Proxima Nova',
+                            fontWeight: FontWeight.w400,
+                            height: 0,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
                 for (int i = 0; i < 50; i++) CustomContainer(index: i),
               ],
             ),
