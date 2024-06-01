86b2e92b27fda4dbbc1947b516c4d2c509a6acce
kenresoft <amadikenneth8@gmail.com>
2024-06-01 03:37:03 +0100

    Modified expanded_header.dart

--- lib/views/widgets/expanded_header.dart
+++ lib/views/widgets/expanded_header.dart
@@ -1,12 +1,16 @@
+import 'package:extensionresoft/extensionresoft.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 
 class ExpandedHeader extends StatelessWidget {
-  const ExpandedHeader({super.key});
+  const ExpandedHeader({super.key, required this.animation});
+
+  final Animation<double> animation;
 
   @override
   Widget build(BuildContext context) {
+    // dev.log(animation.value.toString());
     return Container(
       height: 75.h,
       width: 1.sw,
@@ -16,38 +20,97 @@ class ExpandedHeader extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'The Weeknd',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 22.sp,
-                  fontFamily: 'Proxima Nova',
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.symmetric(vertical: 3),
-                child: Text(
-                  'Community • +11K Members',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 12.sp,
-                    fontFamily: 'Proxima Nova',
-                    fontWeight: FontWeight.w600,
+          AnimatedBuilder(
+            animation: animation,
+            builder: (context, child) {
+              final double space16 = Tween<double>(begin: 0, end: 16.w).evaluate(animation);
+              final double space8 = Tween<double>(begin: 0, end: 8.w).evaluate(animation);
+              final double space6 = Tween<double>(begin: 0, end: 6.h).evaluate(animation);
+              final double arrowSize = Tween<double>(begin: 0, end: 24.w).evaluate(animation);
+              final double imageSize = Tween<double>(begin: 0, end: 50.w).evaluate(animation);
+              final double radius = Tween<double>(begin: 0, end: 200).evaluate(animation);
+
+              return Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Opacity(
+                    opacity: animation.value,
+                    child: Container(
+                      width: arrowSize,
+                      height: arrowSize,
+                      decoration: ShapeDecoration(
+                        color: Colors.black.withOpacity(0.1),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20).r,
+                        ),
+                      ),
+                      child: SvgPicture.asset('assets/svg/arrow-left.svg'),
+                    ),
                   ),
-                ),
-              ),
-            ],
+                  space16.spX,
+                  Opacity(
+                    opacity: animation.value,
+                    child: Container(
+                      width: imageSize,
+                      height: imageSize,
+                      clipBehavior: Clip.antiAlias,
+                      decoration: ShapeDecoration(
+                        color: const Color(0xFFFACB31),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(radius * animation.value).r,
+                        ),
+                      ),
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('assets/img/header-image.png'),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  space8.spX,
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'The Weeknd',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 22.sp,
+                          fontFamily: 'Proxima Nova',
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      space6.spY,
+                      Container(
+                        padding: const EdgeInsets.symmetric(vertical: 3),
+                        child: Text(
+                          'Community • +11K Members',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14.sp,
+                            fontFamily: 'Proxima Nova',
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              );
+            },
           ),
           Container(
             height: 30.h,
             width: 30.w,
-            padding: EdgeInsets.symmetric(horizontal: 7.w, vertical: 7.h),
+            padding: EdgeInsets.symmetric(horizontal: 4.w, vertical: 4.h),
             clipBehavior: Clip.antiAlias,
             decoration: ShapeDecoration(
               shape: RoundedRectangleBorder(
